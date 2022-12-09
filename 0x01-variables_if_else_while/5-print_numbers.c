cf5365cf095feebfc706492c063637bc5443572b
Kalab Solomon <kalab27solomon@gmail.com>
2022-12-09 13:26:46 -0800

    print single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
+}
