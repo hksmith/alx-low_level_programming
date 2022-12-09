80b24ad779781f86fa3bd44c3960290245a9413f
Kalab Solomon <kalab27solomon@gmail.com>
2022-12-09 12:40:42 -0800

    last digit1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - entry point
- * 
- * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
