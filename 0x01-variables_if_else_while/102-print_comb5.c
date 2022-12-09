206c07321783edc5e1c820cb64d21b5bc8b209ee
Kalab Solomon <kalab27solomon@gmail.com>
2022-12-09 15:12:38 -0800

    combination of two digits with space

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int num, num2;
+
+	for (num = '0'; num <= '98'; num++)
+	{
+		for (num2 = num + 1; num2 <= '98'; num2++)
+		{
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 / 10) + '0');
+
+			if (num == 98 && num2 == 99)
+			{
+				continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
