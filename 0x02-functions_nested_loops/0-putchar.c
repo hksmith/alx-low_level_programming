89f43840b6312ebdfa6663a4d7cf3e70673613f3
Kalab Solomon <kalab27solomon@gmail.com>
2022-12-13 06:33:07 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include<unists.h>
+#include "main.h"
 
-/*
- * main - print with writ function
- *
- * Return  always (0).
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	write(1, "_putchar", 9);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
