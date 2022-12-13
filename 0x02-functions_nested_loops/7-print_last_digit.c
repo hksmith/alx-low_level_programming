cc6a656bd5d583300e20e8267b79b81838517f6a
Kalab Solomon <kalab27solomon@gmail.com>
2022-12-13 13:24:29 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,7 @@ int print_last_digit(int n)
 {
 	int a;
 
-	int (n < 0)
+	if (n < 0)
 		n = -n;
 
 	a = n % 10;
@@ -17,7 +17,7 @@ int print_last_digit(int n)
 	if (a < 0)
 		a = -a;
 
-	_puchar(a + '0');
+	_putchar(a + '0');
 
 	return (a);
 }
