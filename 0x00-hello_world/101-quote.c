5ce9154829c2e02029f4e871d051d554cff22a81
kalab solomon <105478362+hksmith@users.noreply.github.com>
2022-12-13 16:37:24 +0300

    print with out printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
+ * main - Prints with write function.
+ *           
  * Return: Always 1.
  */
 
