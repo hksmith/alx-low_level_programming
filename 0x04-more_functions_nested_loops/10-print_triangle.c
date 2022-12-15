010b7962f1cd56747860f07da84d41e88dfa9f2d
Kalab Solomon <kalab27solomon@gmail.com>
2022-12-15 05:23:33 -0800

    print trangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,13 +16,13 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j <= size; j++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			for (j = 1; j <= 1; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
