2ad6126c04dcfd5edf3e53ed7a603c7375ade036
Kalab Solomon <kalab27solomon@gmail.com>
2022-12-15 01:18:30 -0800

    print no 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print no 0-9
+ * it takes no parameters
+ * Void : no return types
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for(i = '0'; i < '10'; i++)
+		putchar(i);
+
+	putchar('\n');
+}
