eb4b1f53c38254b10b4c509461785d65491d0c1e
Kalab Solomon <kalab27solomon@gmail.com>
2022-12-15 02:37:06 -0800

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_squre - print squre n times
+ * @size : parameter no of #
+ * void : no value is retured
+ */
+
+void print_diagonal(int size)
+{
+	int i , j;
+
+	if(size > 0)
+	{
+		for(i = 0; i < size; i++)
+		{
+			for(j = 0; j < size; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
