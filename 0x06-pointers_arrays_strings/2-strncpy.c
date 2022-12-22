2f8c9acac75ba09d856bd946e67b6682d5451433
Kalab Solomon <kalab27solomon@gmail.com>
2022-12-22 11:39:38 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
 	i = 0;
 	
-	while (src[j] != '\0' && i < n)
+	while (src[i] != '\0' && i < n)
 		{
 			dest[i] = src[i];
 			i++;
