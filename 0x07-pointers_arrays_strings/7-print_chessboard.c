2cafa48da2d3dd164a8916e44e8b186fdc21579e
Kalab Solomon <kalab27solomon@gmail.com>
2023-01-02 14:02:04 -0800

    prints chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#incude "main.h"
+
+/**
+ * print_chessboard - prints chess board
+ * @a: two dimension array to print
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putcahr('\n');
+		}
+		_putchar('\n\);
+	}
+}
