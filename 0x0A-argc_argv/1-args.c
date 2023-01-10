c3a818d6867dadd54c5eba8b82277b6e6a04b71f
Kalab Solomon <kalab27solomon@gmail.com>
2023-01-09 21:30:33 -0800

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: a variable that holds the no of the comand used to run the program
+ * @argv: a array that holds the strigs in argc
+ *
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
