5ebe968563689143ab36e4b14cb716572862218b
Kalab Solomon <kalab27solomon@gmail.com>
2023-01-09 21:25:17 -0800

    adding readme file and task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,13 @@
  * main - prints the name of the program
  * @argc: a variable that holds the no of the comand used to run the program
  * @argv: a array that holds the strigs in argc
- * Return: always 0;
+ *
+ * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
+
 	return (0);
 }
