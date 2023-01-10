e54c1a6156b67b42173ab6f0dd058567014a223a
Kalab Solomon <kalab27solomon@gmail.com>
2023-01-09 21:45:06 -0800

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: a variable that holds the no of the comand used to run the program
+ * @argv: a array that holds the strigs in argc
+ *
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+{
+	int i;
+
+	for (i = 1; i < arc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
