9b4faea88c8fa45818a3936bcebd68f6372391d8
kalab solomon <105478362+hksmith@users.noreply.github.com>
2023-01-17 10:15:39 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,33 +1,30 @@
-#ifndef DOG_H
-#define DOG_H
-
-/*
- * File: dog.h
- * Desc: Header file that defines a new type struct dog.
- */
+#ifndef DOG
+#define DOG
 
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - dog class
  *
- * Description: Longer description
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-
-typedef struct dog dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
