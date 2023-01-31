31d7174a83c25eb0b4a9c3c0538567d89b1c6a1e
Kalab Solomon <kalab27solomon@gmail.com>
2023-01-31 11:53:43 -0800

    adding task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the members in the list
+ * @h: a variable that holds the value to be printed
+ *
+ * Return: number of the node
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->);
+		x++;
+		h = h->next;
+	}
+	return(x);
+}
